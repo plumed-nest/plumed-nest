ad07f4eb5fe73628f386e16160a6e83e7a5ad8f4
Massimiliano Bonomi <massimiliano.bonomi@gmail.com>
2019-04-06 18:33:53 +0200

    add spaces

--- nest.py
+++ nest.py
@@ -73,8 +73,10 @@ def cd(newdir):
 with open("list.md","w") as o:
     print("Browse the nest", file=o)
     print("-----------------------------", file=o)
-    print("PLUMED-NEST provides all the data needed to reproduce the results of a PLUMED-enhanced molecular dynamics simulation or analysis contained in a published paper. Furthermore, PLUMED-NEST monitors the compatibility of the provided PLUMED input files with the current version of the code and integrates links from these files to the PLUMED manual.  ", file=o)
-    print("Here is a list of the projects already deposited on PLUMED-NEST:  ", file=o)
+    print("PLUMED-NEST provides all the data needed to reproduce the results of a PLUMED-enhanced molecular dynamics simulation or analysis contained in a published paper. Furthermore, PLUMED-NEST monitors the compatibility of the provided PLUMED input files with the current version of the code and integrates links from these files to the PLUMED manual.", file=o)
+    print("  ", file=o)
+    print("Here is a list of the projects already deposited on PLUMED-NEST:", file=o)
+    print("  ", file=o)
     print("| project     | category | keywords | author(s) |", file=o) 
     print("|:--------:|:---------:|:---------:|:---------:|",   file=o)
 
